5c6276295747b007caf433b33ceefb10f54c20cc
Sergei Ovchinnikov <s.y.ovchinnikov@anoitr.ru>
2021-02-28 18:43:50 +0300

    yess

---
+++ src/test/java/DebitCardTest.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DebitCardTest {
+    @Test
+    void shouldSuccessSubmit() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[name=name]").setValue("Василий Алибабаев");
+        form.$("[name=phone]").setValue("+79271234567");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
+
